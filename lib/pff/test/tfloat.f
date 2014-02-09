93fa3c0405f8db5bcf6518d1ab5922588e2ef591
David Seidel <dbseidel@users.sourceforge.net>
2014-02-09 17:34:11 +0000

    Completely revamped to make it non-interactive. The test now uses the values provided by pf_flt_limits and checks to see if it can decode integer triples that stress the provided limits. If the test passes, a message to that effect is printed. If the precision test fails, the code attempts to iteratively relax the limits until the precision test passes, then prints a message with the complete results of the test. Code has also been added to handle the case where a floating-point exception aborts the test before it can report its findings.

--- lib/pff/test/tfloat.f
+++ lib/pff/test/tfloat.f
@@ -24,87 +24,186 @@ c
 C_Groups main standalone
 c***********************************************************************
 c
-      integer   ierr, ipval, ioff10, loop, k, pf2max, pf2min
-      real      fvalm, fvals, fvalb, xmult, pfrmin, prec
-      integer   ival(3),jval(3),kval(3)
+      implicit none
 c
-      ierr = 0
+      integer   ipval, k, cnt
+      real      fvalm, fvals, fvalb, xmult, prec, rmaxinv, del
+      logical   needchk, fail, prec_bad, rmin_bad, exist, doprint
+      logical   rmax_bad
 c
-      ival(1) = 0
-      ival(2) = 0
-c
-      jval(1) = 32767
-      jval(2) = 32767
-c
-      kval(1) = 10922
-      kval(2) = 21845
-c
-      call pf_flt_limits(pf2max, pf2min, pfrmin)
-c
-      print *,'Current Macro Values:'
-      print *,'MAX_EXP: ',PF2MAX
-      print *,'MIN_EXP: ',PF2MIN
-      print *,'FLT_MIN: ',PFRMIN
+      integer pf2min, pf2max
+      real pfrmin, pfrmax
+      common /pfstuff/ pf2min, pf2max, pfrmin, pfrmax
 
+      real getfloat
+
+      call pf_flt_limits(pf2max, pf2min, pfrmin, pfrmax)
 c
-   10 continue
+      print *,'Current Float Limits:'
+      print *,'  MAX_EXP: ',pf2max
+      print *,'  MIN_EXP: ',pf2min
+      print 300,'  FLT_MIN: ',pfrmin,pfrmin
+      print 300,'  FLT_MAX: ',pfrmax,pfrmax
 c
-        print*,'Enter PF2 parameter value'
-        read(*,*,end=20) ipval
-        ival(3) = (ipval + 8192)*2
-        jval(3) = ival(3)
-        kval(3) = ival(3)
+      doprint = .FALSE.
+      inquire(file='CHG_TFLOAT_MAX',exist=exist)
+      if (exist) then
+        print'(a,i3,a,$)','Enter new value for MAX_EXP (<',pf2max,'): '
+        read *,pf2max
+        doprint = .TRUE.
+      endif
 c
-        call pfui2f (.FALSE.,kval,fvalm,ioff10,ierr)
-        if (ierr.ne.0)then
-          print *,' ui2f min error = ',ierr
-          ierr = 0
-        endif
 c
-        call pfui2f (.FALSE.,ival,fvals,ioff10,ierr)
-        if (ierr.ne.0)then
-          print *,' ui2f min error = ',ierr
-          ierr = 0
-        endif
+      fail = .FALSE.
+      ipval = pf2max
+      xmult = 0.5
+      needchk = .TRUE.
+      cnt = 0
+      do while ( needchk )
 c
-        call pfui2f (.FALSE.,jval,fvalb,ioff10,ierr)
-        if (ierr.ne.0)then
-          print *,' ui2f max error = ',ierr
-          ierr = 0
-        endif
-c
-        loop = abs(ipval)
-c
-        if (ipval.le.0) then
-          xmult = 2.0
-        else
-          xmult = 0.5
-        endif
+        fvalm = getfloat(ipval,10922,21845)
+        fvals = getfloat(ipval,0,0)
+        fvalb = getfloat(ipval,32767,32767)
 c
         prec = fvalm
-        do 22 k=1,loop
+        do k=1,ipval
           prec = prec*xmult
-   22   continue
+        end do
 c
-c       prec = prec + 9.313225746e-10
         prec = prec*1.50
 c
-        print *,'Decoded MAX value = ',fvalb
-        print *,'Decoded MIN value = ',fvals
-        print *,'Precision     = ',prec
-        if ( ipval.gt.0 ) then
-          prec = 1.0/fvalb
-          print *,'1.0/MAX value     = ',prec
-          if (prec.ne.0) then
-            print *,'1/(1/MAX) value   = ',1.0/prec
+        prec_bad = prec.ne.1.0
+        rmaxinv = 1.0/fvalb
+        rmin_bad = rmaxinv.gt.pfrmin
+        rmax_bad = fvalb.gt.pfrmax
+
+        if ( prec_bad .OR. rmin_bad .OR. rmax_bad .OR. 
+     &       ipval.ne.pf2max .OR. doprint) then
+          print *,'EXP value:',ipval
+          if ( rmax_bad ) then
+            print 400,'Decoded MAX value = ',fvalb,fvalb,' FAIL'
+            fail = .TRUE.
+          else
+            print 300,'Decoded MAX value = ',fvalb,fvalb
+          endif
+          print 300,'Decoded MIN value = ',fvals,fvals
+          print *,'Precision     = ',prec
+          print 300,'1.0/MAX value     = ',rmaxinv,rmaxinv
+          if (rmaxinv.ne.0) then
+            print *,'1/(1/MAX) value   = ',1.0/rmaxinv
           else
             print *,'1/(1/MAX) value   = INF'
           endif
+          if ( prec_bad ) then
+            print *,'PRECISION - 1: ',prec - 1.0,' FAIL'
+            fail = .TRUE.
+          endif
+          if ( rmin_bad ) then
+            del = pfrmin - rmaxinv
+            if (del.eq.0.0) then
+              print 200, del,pfrmin,rmaxinv
+            else
+              print 100, del
+            endif
+            fail = .TRUE.
+          endif
+          ipval = ipval - 1
+          cnt = cnt + 1
+          if (cnt.gt.5) then
+            needchk = .FALSE.
+            print *,'**** SANITY CHECK ****'
+          endif
         endif
+        if (.NOT. prec_bad) needchk = .FALSE.
+      end do
 c
-      goto 10
+      ipval = pf2min
+      xmult = 2.0
+      needchk = .TRUE.
+      cnt = 0
+      do while ( needchk )
 c
-   20 continue
+        fvalm = getfloat(ipval,10922,21845)
+        fvals = getfloat(ipval,0,0)
+        fvalb = getfloat(ipval,32767,32767)
 c
-      stop
+        prec = fvalm
+        do k=1,-ipval
+          prec = prec*xmult
+        end do
+c
+        prec = prec*1.50
+c
+        prec_bad = prec.ne.1.0
+        rmin_bad = fvals.gt.pfrmin
+
+        if ( prec_bad .OR. rmin_bad .OR.
+     &       ipval.ne.pf2min .OR. doprint ) then
+          print *,'EXP value:',ipval
+          print 300,'Decoded MAX value = ',fvalb,fvalb
+          print 300,'Decoded MIN value = ',fvals,fvals
+cc          print *,'Decoded MIN value = ',fvals, pfrmin - fvals
+          print *,'Precision     = ',prec, prec - 1.0
+          if ( prec_bad ) then
+            print *,'PRECISION - 1: ',prec - 1.0,' FAIL'
+            fail = .TRUE.
+          endif
+          if ( rmin_bad ) then
+            del = pfrmin - fvals
+            if (del.eq.0.0) then
+              print 200, del,pfrmin,fvals
+            else
+              print 100, del
+            endif
+            fail = .TRUE.
+          endif
+          ipval = ipval + 1
+          cnt = cnt + 1
+          if (cnt.gt.5) then
+            needchk = .FALSE.
+            print *,'**** SANITY CHECK ****'
+          endif
+        endif
+        if (.NOT. prec_bad) needchk = .FALSE.
+      end do
+
+      if (fail) then
+        print *,'**** TFLOAT FAILED ****'
+      else
+        print *,'TFLOAT PASSED'
+      endif
+
+ 100  format(' FLT_MIN - 1.0/MAX:',1pe17.9,'  FAIL')
+ 200  format(' FLT_MIN - 1.0/MAX:',1pe17.9,'  ',2z10,'  FAIL')
+ 300  format(a,1pe15.8,'  0x',z8.8)
+ 400  format(a,1pe15.8,'  0x',z8.8,a)
+
+      end
+
+      real function getfloat(i2,a1,a2)
+
+      implicit none
+
+      integer i2, a1, a2
+
+      double precision RTWO15
+      parameter ( RTWO15 = 1.d0/32768.0d0 )
+
+      integer i2m, i2x, i2rest
+      real xsign, twooff
+
+      integer pf2min, pf2max
+      real pfrmin, pfrmax
+      common /pfstuff/ pf2min, pf2max, pfrmin, pfrmax
+
+c     This is the basics of the algorithm from PFUI2F
+      xsign = 1.0
+      i2m   = i2 - 1
+      i2x    = max( i2m, pf2min + 2 )
+      i2rest = i2m - i2x
+      xsign  = xsign*(2.**i2rest)
+      twooff = 2.**i2x
+      getfloat = xsign*( ( (a2*RTWO15 + a1)*RTWO15 + 1.d0 ) * twooff )
+
+      return
       end
